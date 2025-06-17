c3496f3c3b71fdeed800708153a24d991035b0f1
Stacy Njoki Mwangi <njoki.mwangi@strathmore.edu>
2025-06-17 08:04:57 +0300

    Sorting and reversing order of array items

--- ArrayChallenge/ArraySortReverse.java
+++ ArrayChallenge/ArraySortReverse.java
@@ -1,4 +1,29 @@
 package ArrayChallenge;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+
 public class ArraySortReverse {
+    public static void main(String[] args){
+        //Initial array of pallet IDs
+        String[] pallets={"B54", "A42","B65", "A76"};
+
+        //Sorting process
+        System.out.println("Sortedd....");
+        Arrays.sort(pallets);
+
+        for (String pallet:pallets){
+            System.out.println("--"+ pallet);
+        }
+
+        //Reversal
+        System.out.println("\n Reversed......");
+        List<String> palletList = Arrays.asList(pallets);
+        Collections.reverse(palletList);
+
+        for (String pallet: palletList){
+            System.out.println("--"+ pallet);
+        }
+    }
 }
